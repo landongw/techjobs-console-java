60d5fd5963ecdd4e16b5b0983f44997463cfa2ca
landongw <landon@extantpublishing.com>
2017-11-08 18:11:56 -0600

    Fix typo. Add printJobs method

--- src/org/launchcode/techjobs/console/TechJobs.java
+++ src/org/launchcode/techjobs/console/TechJobs.java
@@ -111,6 +111,19 @@ public class TechJobs {
     // Print a list of jobs
     private static void printJobs(ArrayList<HashMap<String, String>> someJobs) {
 
-        System.out.println("printJobs is not implemented yet");
+        if (someJobs.size() != 0) {
+
+            for (HashMap<String, String> job : someJobs) {
+                System.out.println("*****\n" +
+                        "position type: " + job.get("position type") + "\n" +
+                        "name: " + job.get("name") + "\n" +
+                        "employer: " + job.get("employer") + "\n" +
+                        "location: " + job.get("location") + "\n" +
+                        "core competency: " + job.get("core competency") + "\n" +
+                        "*****\n\n");
+            }
+        } else {
+            System.out.println("No matches.  Please alter your search criteria and try again.");
+        }
     }
 }
